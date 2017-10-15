420b2839ce6ce14d3e67a01f1614983077a167cf
Constantina <polyccon@gmail.com>
2017-10-15 16:19:25 +0100

    sum all primes comments and naming

--- intermediate/15_sum_all_primes.js
+++ intermediate/15_sum_all_primes.js
@@ -27,7 +27,7 @@ function sumPrimes(num) {
   return sum;
 }
 
-sumPrimes(10);
-sumPrimes(10) should return a number.
-sumPrimes(10) should return 17.
-sumPrimes(977) should return 73156.
+
+sumPrimes(10); //should return a number.
+sumPrimes(10); //should return 17.
+sumPrimes(977); //should return 73156.
