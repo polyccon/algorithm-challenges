24390a62edd35564d87ea9128e39bc47c664e91f
Constantina <polyccon@gmail.com>
2017-10-15 21:15:08 +0100

    18 drop it -intermediate

---
+++ intermediate/18_drop_it.js
@@ -0,0 +1,30 @@
+// Drop the elements of an array (first argument), starting from the front,
+//until the predicate (second argument) returns true.
+
+// The second argument, func, is a function you'll use to test the first elements
+//of the array to decide if you should drop it or not.
+
+//Return the rest of the array, otherwise return an empty array.
+
+function dropElements(arr, func) {
+  // Drop them elements.
+  var indexes = [];
+   arr.map( function(item, index) {
+    if (func(item)){
+      indexes.push(index);
+    }
+  });
+  if (indexes.length !==0 ){
+    return arr.slice(indexes[0]);
+  }else {
+    return [];
+  }
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; }); //should return [1,2,3].
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) //should return [3, 4].
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}) //should return [1, 0, 1].
+dropElements([1, 2, 3], function(n) {return n > 0;}) //should return [1, 2, 3].
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}) //should return [].
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) //should return [7, 4].
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) //should return [3, 9, 2].
