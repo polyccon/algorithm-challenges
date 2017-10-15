22554402c46cd4e54abc4e5e360ad643135f8992
Constantina <polyccon@gmail.com>
2017-10-15 15:46:01 +0100

    added tests

--- intermediate/2_diff_two_arrays.js
+++ intermediate/2_diff_two_arrays.js
@@ -16,6 +16,11 @@ function diffArray(arr1, arr2) {
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
-diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
-diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"],["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); //should return an array.
+diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]); //should return ["pink wool"].
+diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]); //should return ["diorite", "pink wool"].
+diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]); //should return [].
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); //should return [4].
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]); //should return ["piglet", 4].
+diffArray([], ["snuffleupagus", "cookie monster", "elmo"]); //should return ["snuffleupagus", "cookie monster", "elmo"].
+diffArray([1, "calf", 3, "piglet"], [7, "filly"]); //should return [1, "calf", 3, "piglet", 7, "filly"].
