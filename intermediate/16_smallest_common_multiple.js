52f2cf8274f134d01ca42eb75474d75faecf1043
Constantina <polyccon@gmail.com>
2017-10-15 15:36:31 +0100

    comment out 'should return'

--- intermediate/16_smallest_common_multiple.js
+++ intermediate/16_smallest_common_multiple.js
@@ -41,8 +41,8 @@ function smallestCommons(arr) {
     return mult;
 }
 
-smallestCommons([1, 5]) should return a number.
-smallestCommons([1, 5]) should return 60.
-smallestCommons([5, 1]) should return 60.
-smallestCommons([1, 13]) should return 360360.
-smallestCommons([23, 18]) should return 6056820.
+smallestCommons([1, 5]); // should return a number.
+smallestCommons([1, 5]); // should return 60.
+smallestCommons([5, 1]); // should return 60.
+smallestCommons([1, 13]); // should return 360360.
+smallestCommons([23, 18]); // should return 6056820.
