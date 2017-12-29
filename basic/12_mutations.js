f6fa32d43c70b5060d07843151dccebfe8e2aab4
Constantina <polyccon@gmail.com>
2017-12-29 20:19:45 +0000

    12 basic correct

--- basic/12_mutations.js
+++ basic/12_mutations.js
@@ -1,11 +1,8 @@
 // Mutations
 // Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
-
-//For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
-
-//The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
-
-//Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
 
 function mutation(arr) {
   var array = [];
@@ -13,11 +10,9 @@ function mutation(arr) {
   var arr1 = arr[1].toLowerCase().split('');
 
   var checkArr0 = arr0.filter(function(value, index, self) {
-    // remove any duplicates
     return self.indexOf(value) === index;
   });
   var checkArr1 = arr1.filter(function(value, index, self) {
-    // remove any duplicates
     return self.indexOf(value) === index;
   });
   checkArr0.sort();
