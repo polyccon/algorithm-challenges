55080ab0c34fd33c9b48cd19763a1395cc97c97e
Constantina <polyccon@gmail.com>
2018-01-14 12:18:08 +0000

    tidy up instructions basic 6 js

--- basic/6_largest_numbers.js
+++ basic/6_largest_numbers.js
@@ -1,7 +1,9 @@
 // Return Largest Numbers in Arrays
-//Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
-//Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
-
+/*Return an array consisting of the largest number from each provided sub-array.
+For simplicity, the provided array will contain exactly 4 sub-arrays.
+Remember, you can iterate through an array with a simple for loop, and access each
+member with array syntax arr[i].
+*/
 function largestOfFour(arr) {
   return arr.map(littlearr => {
     return Math.max.apply(null, littlearr);
