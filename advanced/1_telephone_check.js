18e01298cc5283684eb3aa2c398f0692f957e866
Constantina <polyccon@gmail.com>
2018-01-14 22:07:28 +0000

    alternative solution with regex 1/advanced/js

--- advanced/1_telephone_check.js
+++ advanced/1_telephone_check.js
@@ -51,6 +51,11 @@ function telephoneCheck(str) {
   return true;
 }
 
+// OR
+function telephoneCheck(str) {
+  return /^(1|1\s)?(\d{3}|\(\d{3}\))(\s|-)?\d{3}(\s|-)?\d{4}$/.test(str);
+}
+
 // TESTS
 console.log(telephoneCheck('555-555-5555')); //should return true.
 console.log(telephoneCheck('1 555-555-5555')); //should return true.
