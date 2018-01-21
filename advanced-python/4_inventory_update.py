24eb3d1bda11a3b80d24bf8324f40bacc5fe3a64
Constantina <polyccon@gmail.com>
2018-01-21 00:43:12 +0000

    added sorting function 4 python

--- advanced-python/4_inventory_update.py
+++ advanced-python/4_inventory_update.py
@@ -4,6 +4,8 @@ Compare and update the inventory stored in a 2D array against a second 2D array 
 Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the
 new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
 '''
+def takeSecond(elem):
+    return elem[1]
 
 def updateInventory(arr1, arr2):
     # All inventory must be accounted for or you're fired!
@@ -13,7 +15,8 @@ def updateInventory(arr1, arr2):
         array1[0] += array2[0]
     if all(arr1[i][1] is not array2[1] for i in range(len(arr1))):
         arr1.append(array2)
-  return arr1
+  return sorted(arr1, key=takeSecond)
+ 
 
  print (len(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"],
  [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"],
